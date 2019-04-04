9686b84f7ab0a90977249071f8fa57805d050a60
Leoli21 <43002601+Leoli21@users.noreply.github.com>
2019-04-04 08:37:13 -0400

    Add files via upload

---
+++ src/Cipher.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Cipher {
+	//ask user for string, ask for shift
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please input a String: ");
+		String str = input.nextLine();
+		String newStr = "";
+		for(int i = 0; i < str.length(); i++)
+		{
+			char letter = str.charAt(i);
+			int number = letter - 'a';
+			number = (number - 3) % 26;
+			letter = (char) number;
+			newStr += letter;
+		}
+		System.out.println(newStr);
+		//char letter = 'c';
+		//int number = letter - 'a';
+		//number = (number + 7) % 26;
+		//System.out.println((char) (number + 'a'));
+		
+		//System.out.println(letter);
+		//int number = letter -'a';
+		//System.out.println(number);
+		//number += 5;
+		//System.out.print((char) (number + 'a'));
+
+	}
+
+}
+ 
