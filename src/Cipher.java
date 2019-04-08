5384a6c3ea2d3d75d0388c14fc467aeb3d197ce4
Leoli21 <43002601+Leoli21@users.noreply.github.com>
2019-04-08 19:37:59 -0400

    Add files via upload

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
 public class Cipher {
-	//When doing modular division, 
-
+	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Please input a String: ");
@@ -13,28 +12,15 @@ public class Cipher {
 			char letter = str.charAt(i);
 			int number = letter - 'a';
 			number = (number - 3) % 26;
-			if (number == -3)
+			if (number == -3 || number == -2 || number == -1)
 			{
-				number +=  ;
-			}
-			else if (number == -2)
-			{
-				number +=4 ;
-			}
-			else if (number == -1)
-			{
-				number += 3;
+				number += 26;
 			}
 			letter = (char) (number + 'a');
 			newStr += letter;
 		}
 		System.out.println(newStr);
-		
-		//System.out.println(letter);
-		//int number = letter -'a';
-		//System.out.println(number);
-		//number += 5;
-		//System.out.print((char) (number + 'a'));
+
 
 	}
 
